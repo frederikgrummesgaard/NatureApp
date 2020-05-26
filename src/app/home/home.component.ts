7dbdccdef5864827ea508f5372508f7dde85b2d3
Frederik <frederikgrummesgaard@gmail.com>
2020-05-26 17:08:14 +0200

    static picture changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,6 @@ import { Component, OnInit } from "@angular/core";
 import { RouterExtensions } from "nativescript-angular/router";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
-import { Label } from 'tns-core-modules/ui/label';
-import { isAndroid } from 'tns-core-modules/platform';
-import { EventData } from "tns-core-modules/ui/page/page";
-import { UserService } from "../shared/services/user.service";
-import { FormControl } from "@angular/forms";
 
 @Component({
     selector: "Home",
@@ -33,10 +28,4 @@ export class HomeComponent implements OnInit {
         });
     }
 
-    onLabelLoaded(args: EventData) {
-        const lbl = args.object as Label;
-        if (isAndroid) {
-            lbl.android.setGravity(17)
-        }
-    }
 }
